a3d81bcd975ec37522da64fe58e93e0da03f5d21
Hosmel Quintana <hosmel@otterpro.com>
2014-07-05 09:34:33 -0600

    refactor apputils

--- app/src/main/java/com/socialimprover/saldotuc/app/AppUtil.java
+++ app/src/main/java/com/socialimprover/saldotuc/app/AppUtil.java
@@ -15,16 +15,12 @@ public class AppUtil {
 
     public static int pxToDp(Context context, int px) {
         DisplayMetrics displayMetrics = context.getResources().getDisplayMetrics();
-        int dp = Math.round(px / (displayMetrics.xdpi / DisplayMetrics.DENSITY_DEFAULT));
-
-        return dp;
+        return Math.round(px / (displayMetrics.xdpi / DisplayMetrics.DENSITY_DEFAULT));
     }
 
     public static int dpToPx(Context context, int dp) {
         DisplayMetrics displayMetrics = context.getResources().getDisplayMetrics();
-        int px = Math.round(dp * (displayMetrics.xdpi / DisplayMetrics.DENSITY_DEFAULT));
-
-        return px;
+        return Math.round(dp * (displayMetrics.xdpi / DisplayMetrics.DENSITY_DEFAULT));
     }
 
     public static boolean isNetworkAvailable(Context context) {
